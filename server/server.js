c48160008cb6c1d825789d1fbc7da248087065a0
cderek <pealstyle@gmail.com>
2019-12-29 16:21:26 +0800

    feat: development modal ssr

--- server/server.js
+++ server/server.js
@@ -2,24 +2,27 @@ const express = require('express');
 const ReactSSR = require('react-dom/server');
 const fs = require('fs');
 const path = require('path');
-const serverEntry = require('../dist/server-entry').default;
 
-const template = fs.readFileSync(
-  path.join(__dirname, '../dist/index.html'),
-  'utf8'
-);
+const isDev = process.env.NODE_ENV === 'development';
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, '../dist')));
-
-app.get('*', function(req, res) {
-  const appString = ReactSSR.renderToString(serverEntry);
-
-  const templateStr = template.replace('<!--app-->', appString);
-
-  res.send(templateStr);
-});
+if (!isDev) {
+  const serverEntry = require('../dist/server-entry').default;
+  const template = fs.readFileSync(
+    path.join(__dirname, '../dist/index.html'),
+    'utf8'
+  );
+  app.use('/public', express.static(path.join(__dirname, '../dist')));
+  app.get('*', function(req, res) {
+    const appString = ReactSSR.renderToString(serverEntry);
+    const templateStr = template.replace('<!--app-->', appString);
+    res.send(templateStr);
+  });
+} else {
+  const devStatic = require('./util/dev-static');
+  devStatic(app);
+}
 
 const host = 'localhost';
 const port = process.PORT || '3000';
---
+++ server/util/dev-static.js
@@ -0,0 +1,58 @@
+const axios = require('axios');
+const path = require('path');
+const MemoryFileSystem = require('memory-fs');
+
+const proxy = require('http-proxy-middleware');
+const ReactDomServer = require('react-dom/server');
+
+const webpack = require('webpack');
+const webpackServerConfig = require('../../config/webpack.config.server');
+
+const getTemplate = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get('http://localhost:8000/public/index.html')
+      .then(res => {
+        resolve(res.data);
+      })
+      .catch(reject);
+  });
+};
+
+const Module = module.constructor;
+
+const mfs = new MemoryFileSystem();
+const serverCompiler = webpack(webpackServerConfig);
+serverCompiler.outputFileSystem = mfs;
+let serverBundle;
+serverCompiler.watch({}, (err, stats) => {
+  if (err) throw err;
+  stats = stats.toJson();
+  stats.errors.length > 0 && stats.errors.forEarch(err => console.error(err));
+  stats.warnings.length > 0 &&
+    stats.warnings.forEach(warn => console.warn(warn));
+
+  const bundlePath = path.join(
+    webpackServerConfig.output.path,
+    webpackServerConfig.output.filename
+  );
+  const bundle = mfs.readFileSync(bundlePath, 'utf-8');
+  const m = new Module();
+  m._compile(bundle, 'server-entry.js');
+  serverBundle = m.exports.default;
+});
+
+module.exports = function handleDevSSR(app) {
+  app.use(
+    '/public',
+    proxy({
+      target: 'http://localhost:8000',
+    })
+  );
+  app.get('*', function(req, res) {
+    getTemplate().then(template => {
+      const content = ReactDomServer.renderToString(serverBundle);
+      res.send(template.replace('<!-- app -->', content));
+    });
+  });
+};
